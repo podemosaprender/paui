837eb7d8b21753d3df4d4c0b1726c7fb561c400c
devel@mauriciocap.com <devel@mauriciocap.com>
2024-07-06 23:22:50 +0000

    TRY: share_target, add missing sw

---
+++ public/sw.js
@@ -0,0 +1,84 @@
+//INFO: custom PWA service worker
+//FROM: https://github.com/GoogleChrome/samples/blob/gh-pages/web-share/src/js/service-worker.js
+
+import {CacheOnly} from 'workbox-strategies';
+import {clientsClaim, skipWaiting} from 'workbox-core';
+import {precacheAndRoute} from 'workbox-precaching';
+import {RangeRequestsPlugin} from 'workbox-range-requests';
+import {registerRoute} from 'workbox-routing';
+
+//import {cacheName, channelName, urlPrefix} from './constants';
+const cacheName= 'media';
+const channelName= 'messages';
+const urlPrefix= '/_media/';
+
+const broadcastChannel = 'BroadcastChannel' in self ? new BroadcastChannel(channelName) : null;
+
+// This event is fired when a user has taken action in the browser to remove
+// an item that was previously added to the content index.
+// In Android Chrome, this is triggered by a deletion from the Downloads screen.
+self.addEventListener('contentdelete', (event) => {
+  const cacheKey = event.id;
+
+  event.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    await cache.delete(cacheKey);
+  })());
+});
+
+const shareTargetHandler = async ({event}) => {
+  if (broadcastChannel) {
+    broadcastChannel.postMessage('Saving media locally...');
+  }
+
+  const formData = await event.request.formData();
+  const mediaFiles = formData.getAll('media');
+  const cache = await caches.open(cacheName);
+
+  for (const mediaFile of mediaFiles) {
+    // TODO: Instead of bailing, come up with a
+    // default name for each possible MIME type.
+    if (!mediaFile.name) {
+      if (broadcastChannel) {
+        broadcastChannel.postMessage('Sorry! No name found on incoming media.');
+      }
+      continue;
+    }
+
+    const cacheKey = new URL(`${urlPrefix}${Date.now()}-${mediaFile.name}`, self.location).href;
+    await cache.put(
+      cacheKey,
+      new Response(mediaFile, {
+        headers: {
+          'content-length': mediaFile.size,
+          'content-type': mediaFile.type,
+        },
+      })
+    );
+  }
+
+  // Use the MIME type of the first file shared to determine where we redirect.
+	const routeToRedirectTo = null; //XXX: let app decide and inform user
+  const redirectionUrl = routeToRedirectTo ? `/#${routeToRedirectTo.href}` : '/';
+
+
+	return Response.redirect(redirectionUrl, 303); //A: After the POST succeeds, redirect to the main page.
+};
+
+const cachedMediaHandler = new CacheOnly({
+  cacheName,
+  plugins: [
+		new RangeRequestsPlugin(), //A: Support for cache requests that include a Range: header.
+  ],
+});
+
+skipWaiting();
+clientsClaim();
+
+// This will be replaced by the list of files to precache by
+// the `workbox injectManifest` build step.
+precacheAndRoute(self.__WB_MANIFEST);
+
+registerRoute( '/_share-target', shareTargetHandler, 'POST');
+
+registerRoute( new RegExp(urlPrefix), cachedMediaHandler);

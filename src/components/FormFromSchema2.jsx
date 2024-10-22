e5bf70cb8b3167ff701b1433eebbd3609bcb2a10
Maxi <mcortes.na@gmail.com>
2024-10-22 17:49:17 -0300

    OK: PrimeReact en vista Form

--- src/components/FormFromSchema2.jsx
+++ src/components/FormFromSchema2.jsx
@@ -2,6 +2,11 @@ import React, { useState, useRef } from "react";
 import { apic_set_file, apic_get_file } from 'src/svc/api'
 
 import { InputText } from './controls/InputText';
+import { InputTextarea } from 'primereact/inputtextarea'
+import { InputText as PrimeInputText } from "primereact/inputtext";
+import { Button } from 'primereact/button'
+import { Accordion, AccordionTab } from 'primereact/accordion';
+import { Toast } from 'primereact/toast';
 
 const xdata= {
 	v_highlighted: true,
@@ -19,15 +24,18 @@ export function FormFromSchema() {
 	const jsonInputRef = useRef(null);
 	const jsonSaveRef = useRef(null);
 
+	const toast = useRef(null);
+
 	const handleParseJSON = () => {
 		const newData = jsonInputRef.current.value;
 		let json;
 		try {
 			json = JSON.parse(newData);
 			setData(json);
+			toast.current.show({ severity: 'success', summary: 'Success', detail: 'JSON parsed successfully' });
 		}
 		catch (e) {
-			alert(`Error parsing JSON: ${e}`);
+			toast.current.show({ severity: 'error', summary: 'Error', detail: `Failed to parse: ${e}` });
 		}
 	}
 
@@ -35,21 +43,31 @@ export function FormFromSchema() {
 	const handleSaveToJSON = async () => {
 		const filename = jsonSaveRef.current.value;
 			await apic_set_file(`${filename}.txt`,  JSON.stringify(data));
-			alert(`saved: ${JSON.stringify(data)}`);
+			toast.current.show({ severity: 'success', summary: 'Success', detail: `Saved to file ${filename}.txt` });
 	}
 
-	return (<div>
+	return (<div style={{padding: "1rem"}}>
 		{/* TODO: Usar PrimeReact */}
-		<div>
-			<textarea ref={jsonInputRef} name="" id=""></textarea>
-			<button onClick={handleParseJSON}>Parse JSON</button>
-		</div>
-		<div>
-			<input ref={jsonSaveRef} type="text" name="" id="" />
-			<button onClick={handleSaveToJSON}>Save to JSON file</button>
-		</div>
-		{ Object.entries(data).map( ([k,v], idx) => (
-			<InputText key={idx} label={k} value={v} setValue={ v => setData({...data,[k]:v}) } />
-		))}
+		<Toast ref={toast}></Toast>
+		<Accordion activeIndex={1}>
+			<AccordionTab header="Import/Export">
+			<div style={{display: "flex", flexDirection: "column", gap: "1rem"}}>
+				<div className="p-inputgroup flex-1">
+					<InputTextarea ref={jsonInputRef}></InputTextarea>
+					<Button onClick={handleParseJSON} label="Parse JSON"/>
+				</div>
+				<div className="p-inputgroup flex-1"> 
+					<PrimeInputText ref={jsonSaveRef} defaultValue="[File path]" />
+					<Button onClick={handleSaveToJSON} label="Save to JSON file"/>
+				</div>
+			</div>
+			</AccordionTab>
+			<AccordionTab header="Form fields">
+			{ Object.entries(data).map( ([k,v], idx) => (
+				<InputText key={idx} label={k} value={v} setValue={ v => setData({...data,[k]:v}) } />
+			))}
+			</AccordionTab>
+		</Accordion>
+
 	</div>)
 }
